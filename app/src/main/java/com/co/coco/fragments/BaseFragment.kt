57a728ce6c6d70531cb3e426aa05caf3b589d6c4
Kadik <kadik@Kadiks.local>
2022-03-11 10:50:47 +0600

    Added fragment with viewBinding

---
+++ app/src/main/java/com/co/coco/fragments/BaseFragment.kt
@@ -0,0 +1,24 @@
+package com.co.coco.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dagger.android.support.DaggerFragment
+
+abstract class BaseFragment : DaggerFragment() {
+
+    abstract val layoutId: Int
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(layoutId, container, false)
+    }
+}
---
+++ app/src/main/java/com/co/coco/fragments/FirstFragment.kt
@@ -0,0 +1,19 @@
+package com.co.coco.fragments
+
+import android.os.Bundle
+import android.view.View
+import com.co.coco.R
+import com.co.coco.databinding.FragmentFirstBinding
+import com.redmadrobot.extensions.viewbinding.viewBinding
+
+class FirstFragment : BaseFragment() {
+    override val layoutId: Int
+        get() = R.layout.fragment_first
+
+    private val binding: FragmentFirstBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.nameTextView.text = "1"
+    }
+}
